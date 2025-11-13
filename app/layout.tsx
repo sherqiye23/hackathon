ffd2f1e4a1a28985312665194a732ff740a07f56
Şərqiyə <145263854+sherqiye23@users.noreply.github.com>
2025-11-13 20:59:50 +0400

    modified layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react"
 import type { Metadata, Viewport } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import { Analytics } from "@vercel/analytics/next"
 import './globals.css'
 
 const _geist = Geist({ subsets: ["latin"] })
@@ -49,7 +48,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans antialiased`}>
         {children}
-        <Analytics />
       </body>
     </html>
   )
